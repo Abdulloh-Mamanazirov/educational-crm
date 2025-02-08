8e489ba41ddb6c915349532c28c314dc2f1d1cc7
Abdulloh <abdullajonmamanazirov@gmail.com>
2025-02-08 22:08:45 +0500

    initialize redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
-import { createRoot } from "react-dom/client";
 import axios from "axios";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import "@ant-design/v5-patch-for-react-19";
 
-import App from "./App.jsx";
 import "./index.css";
+import App from "./App.jsx";
+import { store } from "./redux";
 
 // axios
 const token = sessionStorage.getItem("crm-token");
 // axios.defaults.baseURL = "https://api.jsspm.uz/api";
 if (token) axios.defaults.headers.common["Authorization"] = token;
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
