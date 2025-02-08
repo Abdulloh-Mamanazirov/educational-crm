50cdf38b566ad916e7d92474136731beb7ccc676
Abdulloh <abdullajonmamanazirov@gmail.com>
2025-02-08 22:12:25 +0500

    export all pages

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import { teacherRoutes } from "./teacher/teacherRoutes";
+import { studentRoutes } from "./student/studentRoutes";
+import { adminRoutes } from "./admin/adminRoutes";
+import { directorRoutes } from "./director/directorRoutes";
+import { hiddenRoutes } from "./hidden/hiddenRoutes";
+
+export {
+  adminRoutes,
+  studentRoutes,
+  teacherRoutes,
+  directorRoutes,
+  hiddenRoutes,
+};
