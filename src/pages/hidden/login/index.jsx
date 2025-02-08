866c40c4ec552c7c9c73fd3fcdddaca802c7724b
Abdulloh <abdullajonmamanazirov@gmail.com>
2025-02-08 22:19:11 +0500

    change dark mode bg color

--- src/pages/hidden/login/index.jsx
+++ src/pages/hidden/login/index.jsx
@@ -80,7 +80,7 @@ const Login = () => {
         <p className="text-white text-xl font-bold">Educational CRM</p>
       </div>
       {/* Right Side - Form */}
-      <div className="flex flex-col w-full lg:w-1/2 justify-center items-center bg-white dark:bg-dark-l">
+      <div className="flex flex-col w-full lg:w-1/2 justify-center items-center bg-white dark:bg-ant-dark-blue">
         <img src="./favicon.svg" alt="Logo" className="w-32 mb-8" />
         {/* Login Form */}
         <Form
