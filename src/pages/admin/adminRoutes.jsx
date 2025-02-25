7123bfdf05305978eba4f14bd7e911c5f1f28149
Abdulloh <abdullajonmamanazirov@gmail.com>
2025-02-25 17:12:37 +0500

    add pages to the routes

--- src/pages/admin/adminRoutes.jsx
+++ src/pages/admin/adminRoutes.jsx
@@ -1,5 +1,15 @@
-import AdminDashboard from "./home";
 import AdminProfile from "./profile";
+import AdminDashboard from "./home";
+import AdminTeachers from "./teachers";
+import AdminStudents from "./students";
+import AdminEmployees from "./employees";
+import AdminGroups from "./groups";
+import AdminGroupDetails from "./groupDetails";
+import AdminStudentPayments from "./payments";
+import AdminSalaries from "./salaries";
+import AdminExpenses from "./expenses";
+import AdminBadges from "./badges";
+import AdminAnnouncements from "./announcements";
 
 export const adminRoutes = [
   {
@@ -11,17 +21,96 @@ export const adminRoutes = [
     showInMenu: true,
     roles: ["admin"],
   },
-  //   {
-  //     id: 401,
-  //     path: "/admin/users",
-  //     element: <UserManagement />,
-  //     label: "",
-  //     icon: <span className="" />,
-  //     showInMenu: true,
-  //     roles: ["admin"],
-  //   },
+  {
+    id: 401,
+    path: "/admin/teachers",
+    element: <AdminTeachers />,
+    label: "Teachers",
+    icon: <span className="fa-solid fa-chalkboard-user" />,
+    showInMenu: true,
+    roles: ["admin"],
+  },
   {
     id: 402,
+    path: "/admin/students",
+    element: <AdminStudents />,
+    label: "Students",
+    icon: <span className="fa-solid fa-user-graduate" />,
+    showInMenu: true,
+    roles: ["admin"],
+  },
+  {
+    id: 403,
+    path: "/admin/employees",
+    element: <AdminEmployees />,
+    label: "Employees",
+    icon: <span className="fa-solid fa-user-tie" />,
+    showInMenu: true,
+    roles: ["admin"],
+  },
+  {
+    id: 404,
+    path: "/admin/groups",
+    element: <AdminGroups />,
+    label: "Groups",
+    icon: <span className="fa-solid fa-people-group" />,
+    showInMenu: true,
+    roles: ["admin"],
+  },
+  {
+    id: 405,
+    path: "/admin/groups/:group_id",
+    element: <AdminGroupDetails />,
+    showInMenu: false,
+    roles: ["admin"],
+  },
+  {
+    id: 406,
+    path: "/admin/payments",
+    element: <AdminStudentPayments />,
+    label: "Payments",
+    icon: <span className="fa-solid fa-money-bill" />,
+    showInMenu: true,
+    roles: ["admin"],
+  },
+  {
+    id: 407,
+    path: "/admin/salaries",
+    element: <AdminSalaries />,
+    label: "Salaries",
+    icon: <span className="fa-solid fa-sack-dollar" />,
+    showInMenu: true,
+    roles: ["admin"],
+  },
+  {
+    id: 408,
+    path: "/admin/expenses",
+    element: <AdminExpenses />,
+    label: "Expenses",
+    icon: <span className="fa-solid fa-hand-holding-dollar" />,
+    showInMenu: true,
+    roles: ["admin"],
+  },
+  {
+    id: 409,
+    path: "/admin/badges",
+    element: <AdminBadges />,
+    label: "Badges",
+    icon: <span className="fa-solid fa-award" />,
+    showInMenu: true,
+    roles: ["admin"],
+  },
+  {
+    id: 410,
+    path: "/admin/announcements",
+    element: <AdminAnnouncements />,
+    label: "Announcements",
+    icon: <span className="fa-solid fa-bullhorn" />,
+    showInMenu: true,
+    roles: ["admin"],
+  },
+  {
+    id: 411,
     path: "/admin/profile",
     element: <AdminProfile />,
     label: "Profile",
