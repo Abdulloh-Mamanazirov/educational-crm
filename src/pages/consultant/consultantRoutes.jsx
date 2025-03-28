d7155b9e653bc6f59f8c0f7ebed9015896fc89f3
Abdulloh <abdullajonmamanazirov@gmail.com>
2025-03-29 00:19:26 +0500

    add pages to the routes

--- src/pages/consultant/consultantRoutes.jsx
+++ src/pages/consultant/consultantRoutes.jsx
@@ -1,22 +1,23 @@
 import ConsultantDashboard from "./home";
+import ConsultantUniversities from "./universities";
 
 export const consultantRoutes = [
   {
     id: 300,
     path: "/consultant/dashboard",
     element: <ConsultantDashboard />,
-    label: "Home",
-    icon: <span className="fa-solid fa-home" />,
+    label: "Universities",
+    icon: <span className="fa-solid fa-university" />,
     showInMenu: true,
     roles: ["consultant"],
   },
-  // {
-  //   id: 301,
-  //   path: "/consultant/courses",
-  //   element: <>Consultant Courses</>,
-  //   label: "Courses",
-  //   icon: <span className="fa-solid fa-book" />,
-  //   showInMenu: true,
-  //   roles: ["consultant"],
-  // },
+  {
+    id: 301,
+    path: "/consultant/universities",
+    element: <ConsultantUniversities />,
+    label: "Find university",
+    icon: <span className="fa-solid fa-search" />,
+    showInMenu: true,
+    roles: ["consultant"],
+  },
 ];
