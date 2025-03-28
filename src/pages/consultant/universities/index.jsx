8e9e55f10bb5ebeda253327da28a81eedc9b8f03
Abdulloh <abdullajonmamanazirov@gmail.com>
2025-03-29 00:18:42 +0500

    create universities page

---
+++ src/pages/consultant/universities/index.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const UniversityPage = () => {
+  return (
+    <div className="container mx-auto px-4 py-8 dark:bg-gray-900 dark:text-white"></div>
+  );
+};
+
+export default UniversityPage;
