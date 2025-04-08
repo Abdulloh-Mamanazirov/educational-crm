b6ea7086d29b35eb5f8b4a2bd08c77b01f0aae47
Abdulloh <abdullajonmamanazirov@gmail.com>
2025-04-08 13:08:34 +0900

    add new pages to the student routes

--- src/pages/student/studentRoutes.jsx
+++ src/pages/student/studentRoutes.jsx
@@ -1,4 +1,6 @@
 import StudentDashboard from "./home";
+import StudentSchedule from "./schedule";
+import StudentAssignments from "./homeworkAndAssignments";
 import StudentProfile from "./profile";
 
 export const studentRoutes = [
@@ -14,14 +16,37 @@ export const studentRoutes = [
   {
     id: 301,
     path: "/student/courses",
-    element: <>Student Courses</>,
-    label: "Courses",
-    icon: <span className="fa-solid fa-book" />,
+    element: <StudentSchedule />,
+    label: "Schedule",
+    icon: <span className="fa-solid fa-clock" />,
     showInMenu: true,
     roles: ["student"],
   },
   {
     id: 302,
+    path: "/student/homework",
+    element: <StudentAssignments />,
+    label: "Homework",
+    icon: <span className="fa-solid fa-briefcase" />,
+    showInMenu: true,
+    roles: ["student"],
+  },
+  {
+    id: 303,
+    path: "/student/duolingo",
+    element: <></>,
+    label: "Duolingo",
+    icon: (
+      <img
+        src="https://schools-cdn.duolingo.com/favicon.ico"
+        className="lg:w-5"
+      />
+    ),
+    showInMenu: true,
+    roles: ["student"],
+  },
+  {
+    id: 3020,
     path: "/student/profile",
     element: <StudentProfile />,
     label: "Profile",
